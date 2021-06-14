c7fea38883248a4f8f3d61f76e62a4e4b8b92c60
matthiasware <matthias.mitterreiter@uni-jena.de>
2021-06-14 15:59:14 +0200

    reworked layers, added tests for stupid tensor demension maching

---
+++ transformer/layers.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+#
+import numpy as np
+
+
+class Embedding(nn.Module):
+    def __init__(self, d_vocabulary, d_model, d_l):
+        """
+            d_vocabulary: size of vocabulary
+            d_model: dimension of embedding
+            d_l: length of input sentences
+        """
+
+        super(Embedding, self).__init__()
+        self.d_vocabulary = d_vocabulary
+        self.d_model = d_model
+        #
+        self.tok_emb = nn.Embedding(d_vocabulary, d_model)  # token embedding
+        self.pos_emb = nn.Embedding(d_l, d_model)  # position embedding
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, x):
+        seq_len = x.size(1)
+        pos = torch.arange(seq_len, dtype=torch.long)
+        # (seq_len,) -> (batch_size, seq_len)
+        pos = pos.unsqueeze(0).expand_as(x)
+        embedding = self.tok_emb(x) + self.pos_emb(pos)
+        return self.norm(embedding)
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_k):
+        super(ScaledDotProductAttention, self).__init__()
+        self.d_k = d_k
+
+    def forward(self, Q, K, V, attn_mask):
+        # without heads:
+        # Q (d_b, d_l, d_k)
+        # K (d_b, d_l, d_k)
+        # V (d_n, d_l, d_v)
+        # attn_mask (d_b, d_l, d_l)
+        #
+        # with heads
+        # Q (d_b, n_h, d_l, d_k)
+        # K (d_b, n_h, d_l, d_k)
+        # V (d_b, n_h, d_l, d_v)
+        # attn_mask (d_b, n_h, d_l, d_v)
+
+        # scores | attn (d_b, (d_h), d_l, d_l)
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(self.d_k)
+        # Fills elements of self tensor with value where mask is one.
+        scores.masked_fill_(attn_mask, -1e9)
+        attn = nn.Softmax(dim=-1)(scores)
+
+        # context = (d_b, (d_h), d_l, d_v)
+        context = torch.matmul(attn, V)
+        return context, attn
+
+
+class NormedResidualSubLayerConnection(nn.Module):
+    def __init__(self, d_model):
+        super(NormedResidualSubLayerConnection, self).__init__()
+        self.d_model = d_model
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, x, sublayer):
+        return x + sublayer(self.norm(x))
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, d_k, d_v, n_heads):
+        super(MultiHeadAttention, self).__init__()
+        #
+        self.d_k = d_k
+        self.d_v = d_v
+        self.d_model = d_model
+        self.n_heads = n_heads
+        #
+        self.W_Q = nn.Linear(d_model, d_k * n_heads)
+        self.W_K = nn.Linear(d_model, d_k * n_heads)
+        self.W_V = nn.Linear(d_model, d_v * n_heads)
+        #
+        self.model_sdpa = ScaledDotProductAttention(d_k)
+        self.output_linear = nn.Linear(n_heads * d_v, d_v)
+
+        # here d_v == d_model
+        # rework this module to be independent of the dimensions
+        # or simplify the dimensions
+        self.output_norm = nn.LayerNorm(d_v)
+
+    def forward(self, x, attn_mask):
+        # x         (b, d_l, d_model) = (b, s, m)
+        # attn_mask (b, d_l, d_model)
+        #
+        d_b = x.size(0)
+        #
+        # (b, s, m) x (h, m, k) -> (b, h, m, k)
+        q_s = self.W_Q(x).view(d_b, -1, self.n_heads, self.d_k).transpose(1, 2)
+        k_s = self.W_K(x).view(d_b, -1, self.n_heads, self.d_k).transpose(1, 2)
+
+        # (b, s, m) x (h, m, v) -> (b, h, m, k)
+        v_s = self.W_V(x).view(d_b, -1, self.n_heads, self.d_v).transpose(1, 2)
+
+        # (b, l, l) -> (b, h, l, l)
+        attn_mask_headed = attn_mask.unsqueeze(1).repeat(1, self.n_heads, 1, 1)
+
+        context, attn = self.model_sdpa(q_s, k_s, v_s, attn_mask_headed)
+
+        # (b, h, l, v) -> (b, l, h * v)
+        context = context.transpose(1, 2).contiguous().view(
+            d_b, -1, self.n_heads * self.d_v)
+
+        # (b, l, h*v) - > (b, l, v)
+        output = self.output_linear(context)
+
+        # (b, l, v) -> (b, l, v) where v == d_model right now
+        output = self.output_norm(x + output)
+        return output, attn
---
+++ transformer/utils.py
@@ -0,0 +1,20 @@
+def get_attn_pad_mask(x):
+    # x (b, l)
+    batch_size, d_sentence = x.size()
+    pad_attn_mask = x.data.eq(0).unsqueeze(1)
+
+    # (b, l, l)
+    pad_attn_mask = pad_attn_mask.expand(batch_size, d_sentence, d_sentence)
+    return pad_attn_mask
+
+
+def get_attn_mask(x, n_heads=None):
+    mask = get_attn_pad_mask(x)
+    if n_heads is not None:
+        mask = mask.unsqueeze(1).repeat(1, n_heads, 1, 1)
+    return mask
+
+# def get_attn_pad_mask(x):
+#     batch_size, d_sentence = x.size()
+#     pad_attn_mask = (x == 0).unsqueeze(1).repeat(1, d_sentence, 1)
+#     return pad_attn_mask
